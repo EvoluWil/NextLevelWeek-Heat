a7f20038975c7b6b9a5ad23c853851e242898e8a
Willian-Rodrigues <willian.rsilva1994@gmail.com>
2021-10-20 09:17:51 -0300

    update error response

--- backend-node/src/controllers/MessageController/CreateMessageController.ts
+++ backend-node/src/controllers/MessageController/CreateMessageController.ts
@@ -3,12 +3,14 @@ import { CreateMessageService } from "../../services/MessageService/CreateMessag
 
 export class CreateMessageController {
   async handle(req: Request, res: Response) {
-    const { text } = req.body;
+    const { message } = req.body;
     const { user_id } = req;
-
+    if (!message) {
+      return res.status(401).json({ error: "invalid-message" });
+    }
     const service = new CreateMessageService();
 
-    const result = await service.execute(text, user_id);
+    const result = await service.execute(message, user_id);
 
     return res.json(result);
   }
--- backend-node/src/services/MessageService/GetMessagesService.ts
+++ backend-node/src/services/MessageService/GetMessagesService.ts
@@ -3,7 +3,7 @@ import { prismaClient } from "../../prisma";
 export class GetMessagesService {
   async execute() {
     const messages = await prismaClient.message.findMany({
-      take: 5,
+      take: 3,
       orderBy: {
         created_at: "desc",
       },
